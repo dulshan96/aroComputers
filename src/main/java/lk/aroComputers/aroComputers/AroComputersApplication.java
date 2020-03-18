e4645f7c383d7995b4ba6ac964cc8a488c43e36f
Lositha <asakahatapitiya@gmail.com>
2020-03-19 01:00:00 +0530

    Initial commit

---
+++ src/main/java/lk/aroComputers/aroComputers/AroComputersApplication.java
@@ -0,0 +1,13 @@
+package lk.aroComputers.aroComputers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroComputersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AroComputersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/aroComputers/aroComputers/AroComputersApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.aroComputers.aroComputers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroComputersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
