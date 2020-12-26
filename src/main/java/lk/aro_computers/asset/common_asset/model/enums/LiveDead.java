4999cf3fdf5c7824d067131acb14a86328fd1436
LalithK90 <asakahatapitiya@gmail.com>
2020-12-26 19:40:31 +0530

    Live and Dead was added

---
+++ src/main/java/lk/aro_computers/asset/common_asset/model/enums/LiveDead.java
@@ -0,0 +1,13 @@
+package lk.aro_computers.asset.common_asset.model.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum LiveDead {
+    ACTIVE("Active"),
+    STOP("Stop");
+
+    private final String liveDeath;
+}
